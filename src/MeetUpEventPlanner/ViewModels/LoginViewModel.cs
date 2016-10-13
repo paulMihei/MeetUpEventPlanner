d46fa139bb5d004d41db3418193d08aa64437554
Paul Mihei <paulmihei@gmail.com>
2016-10-13 23:40:10 +0200

    add ? to Remember Me

--- src/MeetUpEventPlanner/ViewModels/LoginViewModel.cs
+++ src/MeetUpEventPlanner/ViewModels/LoginViewModel.cs
@@ -9,7 +9,7 @@ namespace MeetUpEventPlanner.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember Me")]
+        [Display(Name = "Remember Me?")]
         public bool RememberMe { get; set; }
 
         public string ReturnUrl { get; set; }
